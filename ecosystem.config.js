6d7de2685a7ef90aa4ecb917f2f879e3e2abd8f3
大大白 <siyuanxiong2333@gmail.com>
2025-01-01 19:47:09 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,8 @@ module.exports = {
         name: "bgp-query-bot",
         script: "bun",
         args: "run start",
-        interpreter: "bun",
         env: {
             NODE_ENV: "production",
-            PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
         }
     }]
 }
